014a064446fa8ac99448c8966997221c44c33a7e
Aryan Bahl <aryan1658.be21@chitkara.edu.in>
2024-04-16 03:41:22 +0530

    Optimised Mobile User Interface

--- src/components/SelectionBtn.jsx
+++ src/components/SelectionBtn.jsx
@@ -2,7 +2,7 @@ import { ColorPicker, Slider } from 'antd'
 import React from 'react'
 const SelectionBtn = ({ text, property, setProperty, type }) => {
     return (
-        <div className='flex gap-3 items-center bg-[#9DB7CD] p-2 rounded-md' >
+        <div className='flex gap-3 items-center justify-around bg-[#9DB7CD] p-2 rounded-md' >
             <span>Select {text}: </span>
             {
                 type === 'color' && <ColorPicker value={property} onChange={(e) => {
